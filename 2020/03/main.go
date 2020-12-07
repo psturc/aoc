af465d542af4d6242724bcd4748cc73c0585c27a
Pavel Sturc <psturc@redhat.com>
2020-12-07 23:18:30 +0100

    2020: day 3

--- 2020/03/main.go
+++ 2020/03/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var treesMet int
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fileLines := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+	treesMet = treesHitsWithSlope(fileLines, 3, 1)
+	fmt.Println("part one: ", treesMet)
+
+	treesMet *= treesHitsWithSlope(fileLines, 1, 1)
+	treesMet *= treesHitsWithSlope(fileLines, 5, 1)
+	treesMet *= treesHitsWithSlope(fileLines, 7, 1)
+	treesMet *= treesHitsWithSlope(fileLines, 1, 2)
+
+	fmt.Println("part two: ", treesMet)
+}
+
+func treesHitsWithSlope(fileLines []string, xStep int, yStep int) (treesHits int) {
+	xPosition := 0
+	yPosition := 0
+
+	lineLength := len(fileLines[0])
+
+	for i, line := range fileLines {
+
+		if i%yStep != 0 {
+			continue
+		}
+
+		if string(line[xPosition%lineLength]) == "#" {
+			treesHits++
+		}
+
+		xPosition += xStep
+		yPosition += yStep
+	}
+	return
 }
